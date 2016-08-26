f6b8df9726e0af109fee0e5c7bdc4a66c967885e
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-08-25 17:01:27 -0700

    Add ASCCI art

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,38 @@
-class HelloWorld {
-	
+public class HelloWorld {
+
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		p(" __________ ");
+		p("|          |");
+		p("| ( o) ( o)|");
+		p("|     ^    |");
+		p("|   ====== |");
+		p("|__________|");
+		p("     _| |_____");
+		p("    |         |");
+		p("    | ||   || |");
+		p("    | ||   || |");
+		p("    | \\/   \\/ |");
+		p("    |_________|");
+		p("    |---------|");
+		p("    |         |");
+		p("    |    |    |");
+		p("    |    |    |");
+		p("    |    |    |");
+		p("    |    |^^^^|");
+		p("    |    |");
+		p("    |^^^^|");
+	}
+
+	private static void p(String text) {
+		for (int i = 0; i < text.length(); i++) {
+			System.out.print(text.charAt(i));
+			try {
+				Thread.sleep(15);
+			} catch (Exception e) {
+				System.out.println("You can't take the sky from me, " + e);
+			}
+			
+		}
+		System.out.println("");
 	}
 }
