0b121f132833f5346ba11f726cbea66c2b39549a
Michael MacLeod <michaelmmacleod@gmail.com>
2016-09-16 14:55:23 -0700

    Add the WhileLoopLabs project

---
+++ WhileLoopLabs/lab09i.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class lab09i {
+
+	/*/
+	 * Michael MacLeod
+	 * Created 9/16/2016
+	/*/
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int num;
+		int base;
+		int baseTest = 0;
+
+		System.out.print("Enter a base 10 number: ");
+		ans = scan.nextInt();
+		System.out.println("Enter a base: ");
+		base = scan.nextInt();
+		while (base * bastText <= num) {
+			
+		}
+
+	}
+}
