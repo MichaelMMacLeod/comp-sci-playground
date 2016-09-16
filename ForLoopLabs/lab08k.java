001ed95df1aa7582898fb13ca3a2de0fb664e685
Michael MacLeod <michaelmmacleod@gmail.com>
2016-09-16 14:47:36 -0700

    Add an aditional test case

--- ForLoopLabs/lab08k.java
+++ ForLoopLabs/lab08k.java
@@ -8,11 +8,11 @@ public class lab08k {
     public static void main(String[] args) {
 
         int tempAns = 0; // Holds temporary values
-        final int[] data1 = {5, 4, 8, 15, 9, 3};
+        final int[] data1 = {5, 4, 8, 30, 9, 3};
         final int[] data2 = {25, 400, 80, 45, 9, 543};
 
         for (int i = 0; i < 6; i++) {
-            for (int j = 1; j < data2[i]; j++) {
+            for (int j = 1; j <= data2[i]; j++) {
                 if (data1[i] % j == 0 && data2[i] % j == 0) {
                     tempAns = j;
                 }
