4c883c26e4103c4d9883e98026f170844c2ba48a
Michael MacLeod <michaelmmacleod@gmail.com>
2016-09-16 14:38:24 -0700

    Change variable name from carDoor to tempAns

--- ForLoopLabs/lab08k.java
+++ ForLoopLabs/lab08k.java
@@ -7,17 +7,17 @@ public class lab08k {
 
     public static void main(String[] args) {
 
-        int carDoor = 0; // Holds temporary values
+        int tempAns = 0; // Holds temporary values
         final int[] data1 = {5, 4, 8, 15, 9, 3};
         final int[] data2 = {25, 400, 80, 45, 9, 543};
 
         for (int i = 0; i < 6; i++) {
             for (int j = 1; j < data2[i]; j++) {
                 if (data1[i] % j == 0 && data2[i] % j == 0) {
-                    carDoor = j;
+                    tempAns = j;
                 }
             }
-            System.out.println("The gcd of " + data1[i] + " and " + data2[i] + " is " + carDoor);
+            System.out.println("The gcd of " + data1[i] + " and " + data2[i] + " is " + tempAns);
         }
     }
 }
