92d1cbb17460c92cad832aae08a455fcf41d0e93
Michael MacLeod <michaelmmacleod@gmail.com>
2016-09-20 14:54:41 -0700

    Refactor base conversion project and fix overflow errors

--- WhileLoopLabs/lab09i.java
+++ WhileLoopLabs/lab09i.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import static java.lang.Math.pow;
 
 public class lab09i {
 
@@ -12,12 +11,7 @@ public class lab09i {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int num = 0;
-        int numRemainder;
-        int reqBase = 0;
-        int currentPow = 0;
-        int powCount = 0;
-        int currentDigit = 0;
+        int num = 0, base = 0, power = 1;
         ArrayList<Integer> ans = new ArrayList<Integer>();
         String[] extraNums = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
@@ -27,45 +21,46 @@ public class lab09i {
             num = scan.nextInt();
             if (num <= 0) System.out.println("Use a number larger than zero.");
         }
-        while (reqBase < 2) {
+        while (base < 2) {
             System.out.print("Enter a new base: ");
-            reqBase = scan.nextInt();
-            if (reqBase < 2) System.out.println("Use base two or higher.");
+            base = scan.nextInt();
+            if (base < 2) System.out.println("Use base two or higher.");
         }
-        // Find the maximum power of the base which is less than or equal to 
-        // the given number
-        while (reqBase * currentPow <= num) {
-            currentPow = (int) Math.pow(reqBase, powCount);
-            powCount++;
+        // Find max power of base less than or equal to the number
+        while (power * base <= num) {
+            power *= base;
         }
-        // Calculate each digit
-        numRemainder = num;
-        while (currentPow >= 1) {
-            currentDigit = numRemainder / currentPow;
-            numRemainder %= currentPow;
-            ans.add((int) currentDigit); // Ignore the decimals
-            currentPow /= reqBase;
+        // Calculate the digits of the answer
+        int remainder = num;
+        while (power >= 1) {
+            ans.add(remainder / power);
+            remainder %= power;
+            power /= base;
         }
         // Print out answer
-        if (reqBase > extraNums.length) {
-            System.out.println("Since base " + reqBase + " requires more symbols than the alphabet can provide, some digits will be represented in base 10 and separated by braces.");
-        }
-        System.out.print(num + " in base 10 is ");
-        for (int i = 0; i < ans.size(); i++) {
-            if (ans.get(i) > 9 && ans.get(i) - 10 < extraNums.length) { 
-                // Use special characters for bases larger than 10
-                System.out.print(extraNums[ans.get(i) - 10]);
-            } else if (ans.get(i) < 10) { 
-                // Use normal characters for digits 1 through 9
-                System.out.print(ans.get(i));
-            } else { 
-                // Since our array only lists A through Z, it's possible that 
-                // we encounter digits which we can't represent. To fix this, 
-                // we represent them as base 10 numbers separated by braces.
-                System.out.print("{" + ans.get(i) + "}");
+        if (power >= 0) { // Check for overflow
+            if (base > extraNums.length) {
+                System.out.println("Since base " + base + " requires more symbols than the alphabet can provide, some digits will be represented in base 10 and separated by braces.");
             }
+            System.out.print(num + " in base 10 is ");
+            for (int i = 0; i < ans.size(); i++) {
+                if (ans.get(i) > 9 && ans.get(i) - 10 < extraNums.length) { 
+                    // Use special characters for bases larger than 10
+                    System.out.print(extraNums[ans.get(i) - 10]);
+                } else if (ans.get(i) < 10) { 
+                    // Use normal characters for digits 1 through 9
+                    System.out.print(ans.get(i));
+                } else { 
+                    // Since our array only lists A through Z, it's possible that 
+                    // we encounter digits which we can't represent. To fix this, 
+                    // we represent them as base 10 numbers separated by braces.
+                    System.out.print("{" + ans.get(i) + "}");
+                }
+            }
+            System.out.println(" in base " + base + ".");
+            System.out.println("Used " + ans.size() + " digits.");
+        } else {
+            System.out.println("Overflow error");
         }
-        System.out.println(" in base " + reqBase + ".");
-        System.out.println("Used " + ans.size() + " digits.");
     }
 }
