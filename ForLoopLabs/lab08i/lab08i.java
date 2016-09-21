d886dac02efd9036228f8b2fbeb30514687fa361
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-09-21 15:39:15 -0700

    Add lab08i

---
+++ ForLoopLabs/lab08i/lab08i.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class lab08i {
+
+	/*/
+	 * Michael MacLeod
+	/*/
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int ans;
+
+		ans = scan.nextInt();
+		if (isPrime(ans)) {
+			System.out.println(ans + " is prime.");
+		} else {
+			System.out.println(ans + " is not prime.");
+		}
+	}
+
+	private static boolean isPrime(int num) {
+		for (int i = 2; i < num / 2; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
