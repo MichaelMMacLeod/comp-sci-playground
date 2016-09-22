b3d2f60fd19bce0799fe9eb1805809b9e0891678
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-09-21 19:14:13 -0700

    Fix some numbers not working properly

--- ForLoopLabs/lab08i/lab08i.java
+++ ForLoopLabs/lab08i/lab08i.java
@@ -20,7 +20,7 @@ public class lab08i {
 	}
 
 	private static boolean isPrime(int num) {
-		for (int i = 2; i < num / 2; i++) {
+		for (int i = 2; i <= num / 2; i++) {
 			if (num % i == 0) {
 				return false;
 			}
