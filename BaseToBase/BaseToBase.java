858ecb37a4550384994c8defcc6b82c77228fb22
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-09-22 23:04:22 -0700

    Create methods for going to a string array, and moving to base 10
    I'm trying to clean up the main method
    
    TODO: make toBaseTen return an integer array instead of an int,
    just for clarity reasons.

--- BaseToBase/BaseToBase.java
+++ BaseToBase/BaseToBase.java
@@ -4,11 +4,53 @@ import static java.lang.Math.pow;
 
 public class BaseToBase {
 
+	// Holds symbols used in bases 11 through 36
+	public static final String[] extra = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+	/** 
+	 * Returns an array which holds each digit in a different index
+	 * @param number will be turned into an array
+	 * @param base   is the base which the number is in
+	 */
+	public static String[] toStringArray(String number, int base) {
+
+		String[] array;
+
+		if (base > extra.length + 10) {
+			array = number.split(",");
+		} else {
+			array = number.split("");
+		}
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < extra.length; j++) {
+				if (array[i].equalsIgnoreCase(extra[j])) {
+					array[i] = Integer.toString(j + 10);
+				}
+			}
+		}
+		return array;
+	}
+
+	/**
+	 * Returns the given number in base 10
+	 * @param number is an integer array where each digit is in a different index
+	 * @param base   is the base that the number is in
+	 */
+	public static int toBaseTen(int[] number, int base) {
+
+		int base10 = 0;
+
+		for (int i = 0; i < number.length; i++) {
+			base10 += number[number.length - i - 1] * Math.pow(base, i);
+		}
+		return base10;
+	}
+
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
 		ArrayList<Integer> ans = new ArrayList<Integer>();
-		String[] extra = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+		
 		String[] numStrArray;
 		String num;
 		int baseOld, baseNew, numBase10 = 0;
@@ -20,29 +62,19 @@ public class BaseToBase {
 		baseOld = scan.nextInt();
 		System.out.print("New Base: ");
 		baseNew = scan.nextInt();
-		// Move the number into a string array
-		if (baseOld > extra.length + 10) {
-			numStrArray = num.split(",");
-		} else {
-			numStrArray = num.split("");
-		}
-		// Convert letters into numbers
-		for (int i = 0; i < numStrArray.length; i++) {
-			for (int j = 0; j < extra.length; j++) {
-				if (numStrArray[i].equalsIgnoreCase(extra[j])) {
-					numStrArray[i] = Integer.toString(j + 10);
-				}
-			}
-		}
+
+		// Move input into a string array
+		numStrArray = toStringArray(num, baseOld);
+		
 		// Move the number into an int array
 		int[] numArray = new int[numStrArray.length];
 		for (int i = 0; i < numStrArray.length; i++) {
 			numArray[i] = Integer.parseInt(numStrArray[i]);
 		}
+		
 		// Convert to base 10
-		for (int i = 0; i < numArray.length; i++) {
-			numBase10 += numArray[numArray.length - i - 1] * Math.pow(baseOld, i);
-		}
+		numBase10 = toBaseTen(numArray, baseOld);
+
 		// Find the max power of the new base which is less than or equal to the number in base 10
 		int power = 1;
 		while (power * baseNew <= numBase10) {
