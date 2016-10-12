6935243ae370ccdad77198704afb460f0669f5b2
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-10-11 19:22:26 -0700

    Make the game board wrap along edges

--- GameOfLife/GameOfLife.java
+++ GameOfLife/GameOfLife.java
@@ -3,103 +3,92 @@ import java.util.Arrays;
 
 public class GameOfLife {
 
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        boolean[][] map = new boolean[20][20];
-        boolean selecting = true;
+		Scanner scan = new Scanner(System.in);
+		boolean[][] map = new boolean[20][20];
+		boolean selecting = true;
 
-        System.out.println("Enter speed (#/sec)");
-        int interval = scan.nextInt();
-        while (selecting) {
-            System.out.println("Enter seed (x coordinate)");
-            int x = scan.nextInt();
-            System.out.println("Enter seed (y coordinate)");
-            int y = scan.nextInt();
-            try {
-                map[x][y] = true;
-            } catch (Exception e) {
-                selecting = false;
-            }
-            printMap(map);
-        }
-        while (!Arrays.deepEquals(map, step(map))) {
-            map = step(map);
-            printMap(map);
-            try {
-                Thread.sleep(1000 / interval);
-            } catch (Exception e) {}
-        }
-        System.out.println("Done!");
-    }
+		System.out.println("Enter speed (#/sec)");
+		int interval = scan.nextInt();
+		while (selecting) {
+			System.out.println("Enter seed (x coordinate)");
+			int x = scan.nextInt();
+			System.out.println("Enter seed (y coordinate)");
+			int y = scan.nextInt();
+			try {
+				map[x][y] = true;
+			} catch (Exception e) {
+				selecting = false;
+			}
+			printMap(map);
+		}
+		while (!Arrays.deepEquals(map, step(map))) {
+			map = step(map);
+			printMap(map);
+			try {
+				Thread.sleep(1000 / interval);
+			} catch (Exception e) {}
+		}
+		System.out.println("Done!");
+	}
 
-    public static boolean[][] step(boolean[][] oldMap) {
+	public static boolean[][] step(boolean[][] oldMap) {
 
-        boolean[][] map = new boolean[oldMap.length][oldMap.length];
-        
-        for (int i = 0; i < oldMap.length; i++) {
-            for (int j = 0; j < oldMap.length; j++) {
+		boolean[][] map = new boolean[oldMap.length][oldMap.length];
 
-                int neighbours = 0;
+		for (int i = 0; i < oldMap.length; i++) {
+			for (int j = 0; j < oldMap.length; j++) {
 
-                // there has to be a better way
-                try {
-                    if (oldMap[i - 1][j - 1]) neighbours++;
-                } catch (Exception e) {};
-                try {
-                    if (oldMap[i - 1][j + 0]) neighbours++;
-                } catch (Exception e) {};
-                try {
-                    if (oldMap[i - 1][j + 1]) neighbours++;
-                } catch (Exception e) {};
-                try {
-                    if (oldMap[i + 0][j + 1]) neighbours++;
-                } catch (Exception e) {};
-                try {
-                    if (oldMap[i + 1][j + 1]) neighbours++;
-                } catch (Exception e) {};
-                try {
-                    if (oldMap[i + 1][j + 0]) neighbours++;
-                } catch (Exception e) {};
-                try {
-                    if (oldMap[i + 1][j - 1]) neighbours++;
-                } catch (Exception e) {};
-                try {
-                    if (oldMap[i + 0][j - 1]) neighbours++;
-                } catch (Exception e) {};
-                if (neighbours < 2) 
-                    map[i][j] = false;
-                if ((neighbours == 2 || neighbours == 3) && oldMap[i][j] == true)
-                    map[i][j] = true;
-                if (neighbours > 3) 
-                    map[i][j] = false;
-                if (neighbours == 3 && oldMap[i][j] == false)
-                    map[i][j] = true;
-            }
-        }
-        return map;
-    }
+				int neighbours = 0;
+				// Make the game board wrap along edges
+				// left, up, right, down
+				int l = i == 0 ? oldMap.length - 1: i - 1;
+				int u = j == 0 ? oldMap.length - 1: j - 1;
+				int r = i == oldMap.length - 1? 0 : i + 1;
+				int d = j == oldMap.length - 1? 0 : j + 1;
 
-    public static void printMap(boolean[][] map) {
-        System.out.println();
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                if (map[i][j]) {
-                    System.out.print(ANSI_GREEN + "1");
-                } else {
-                    System.out.print(ANSI_WHITE + "0");
-                }
-            }
-            System.out.println(ANSI_WHITE);
-        }
-    }
+				if (oldMap[l][u]) neighbours++;
+				if (oldMap[l][j]) neighbours++;
+				if (oldMap[l][d]) neighbours++;
+				if (oldMap[i][d]) neighbours++;
+				if (oldMap[r][d]) neighbours++;
+				if (oldMap[r][j]) neighbours++;
+				if (oldMap[r][u]) neighbours++;
+				if (oldMap[i][u]) neighbours++;
+				if (neighbours < 2) 
+					map[i][j] = false;
+				if ((neighbours == 2 || neighbours == 3) && oldMap[i][j] == true)
+					map[i][j] = true;
+				if (neighbours > 3) 
+					map[i][j] = false;
+				if (neighbours == 3 && oldMap[i][j] == false)
+					map[i][j] = true;
+			}
+		}
+		return map;
+	}
+
+	public static void printMap(boolean[][] map) {
+		System.out.println();
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				if (map[i][j]) {
+					System.out.print(ANSI_GREEN + "1");
+				} else {
+					System.out.print(ANSI_WHITE + "0");
+				}
+			}
+			System.out.println(ANSI_WHITE);
+		}
+	}
 }
