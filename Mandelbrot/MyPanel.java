962c7daaae9b55fd8ee8bceaebf24e0c08f2421a
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-11-30 21:08:33 -0800

    Increase/decrease number of colors with r/f

--- Mandelbrot/MyPanel.java
+++ Mandelbrot/MyPanel.java
@@ -11,7 +11,7 @@ import javax.swing.JPanel;
 public class MyPanel extends JPanel {
 
 	private BufferedImage canvas;
-	private int width, height, max = 1000, black = 0x000000;
+	private int width, height, max = 250, black = 0x000000;
 	private int[] colors;
 	private double zoom, xShift, yShift;
 	private KeyLis keyLis = new KeyLis(this);
@@ -26,16 +26,22 @@ public class MyPanel extends JPanel {
 
 		canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-		colors = new int[max];
-		for (int i = 0; i < max; i++) {
-			colors[i] = Color.HSBtoRGB(i / 256f, 1, i / (i + 8f));
-		}
+		updateColors();
 
 		zoom = 1;
 		xShift = 0;
 		yShift = 0;
 
 		renderBrot();
+	}
+
+	private void updateColors() {
+
+		colors = new int[max];
+
+		for (int i = 0; i < max; i++) {
+			colors[i] = Color.HSBtoRGB(i / 256f, 1, i / (i + 8f));
+		}
 	}
 
 	private void renderBrot() {
@@ -49,7 +55,7 @@ public class MyPanel extends JPanel {
 				double x = 0, y = 0;
 				int iterations = 0;
 
-				while (x * x + y * y < 3 && iterations < max) {
+				while (x * x + y * y < 10 && iterations < max) {
 
 					double x_new = x * x - y * y + c_re;
 
@@ -85,6 +91,28 @@ public class MyPanel extends JPanel {
 
 		public KeyLis(MyPanel panel) {
 
+			Action increaseColors = new AbstractAction() {
+				public void actionPerformed(ActionEvent e) {
+
+					max += 50;
+					updateColors();
+
+					renderBrot();
+					repaint();
+				}
+			};
+
+			Action decreaseColors = new AbstractAction() {
+				public void actionPerformed(ActionEvent e) {
+
+					if (max > 50) max -= 50;
+					updateColors();
+
+					renderBrot();
+					repaint();
+				}
+			};
+
 			Action zoomIn = new AbstractAction() {
 				public void actionPerformed(ActionEvent e) {
 					zoom *= 0.5;
@@ -115,17 +143,23 @@ public class MyPanel extends JPanel {
 							System.out.println("bad input");
 							break;
 					}
+
 					renderBrot();
+
 					repaint();
 				}
 			};
 
-			panel.getInputMap().put(KeyStroke.getKeyStroke("Q"), "zoomIn");
+			panel.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "zoomIn");
 			panel.getInputMap().put(KeyStroke.getKeyStroke("A"), "move");
 			panel.getInputMap().put(KeyStroke.getKeyStroke("W"), "move");
 			panel.getInputMap().put(KeyStroke.getKeyStroke("D"), "move");
 			panel.getInputMap().put(KeyStroke.getKeyStroke("S"), "move");
+			panel.getInputMap().put(KeyStroke.getKeyStroke("R"), "increaseColors");
+			panel.getInputMap().put(KeyStroke.getKeyStroke("F"), "decreaseColors");
 
+			panel.getActionMap().put("increaseColors", increaseColors);
+			panel.getActionMap().put("decreaseColors", decreaseColors);
 			panel.getActionMap().put("zoomIn", zoomIn);
 			panel.getActionMap().put("move", move);
 		}
