246e86d6432b185ba95d3d8a11368db401b675ac
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-12-05 16:11:29 -0800

    Add the inheritance project

---
+++ Inheritance/FamousPerson.java
@@ -0,0 +1,8 @@
+public class FamousPerson {
+	
+	protected String name, lastName, saying;
+	
+	protected void speak() {
+		System.out.println(saying + " -- " + name + " " + lastName);
+	}
+}
---
+++ Inheritance/Yoda.java
@@ -0,0 +1,8 @@
+public class Yoda extends FamousPerson {
+	
+	@Override
+	public void speak() {
+		System.out.println("")
+	}
+
+}
