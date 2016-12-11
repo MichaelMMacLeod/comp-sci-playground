691a3e04b3ceb445f75e638b92b47af2f6ebb6b7
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-12-11 15:25:36 -0800

    Update factorial program

--- Haskell/functionTest/test.hs
+++ Haskell/functionTest/test.hs
@@ -1,8 +1,6 @@
 fac 0 = 1
 fac n = n * fac (n - 1)
- 
-main = do putStrLn "What is 5! ?"
-          x <- readLn
-          if x == fac 5
-            then putStrLn "You're right!"
-            else putStrLn "You're wrong!"
+
+main = do
+    x <- readLn
+    putStrLn (show (fac x))
