18231cdc24da8fbc99249eaeb0e9ac33c27aa6c0
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-12-12 20:07:49 -0800

    Add a keyListener class

---
+++ Java/InputManager/InputManager.java
@@ -0,0 +1,66 @@
+import javax.swing.Action;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+import javax.swing.JPanel;
+
+public class InputManager {
+
+	JPanel panel;
+	String[] keysValues = new String[0];
+	boolean[] keys = new boolean[0];
+
+	public InputManager(JPanel panel) {
+		this.panel = panel;
+
+		Action pressed = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				e = e.getActionCommand();
+			}
+		};
+		Action released = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				e = e.getActionCommand().substring(9); // "released A" -> "A"
+			}
+		};
+
+		panel.getActionMap().put("pressed", pressed);
+		panel.getActionMap().put("released", released);
+	}
+
+	public boolean pressed(String key) {
+
+	}
+
+	public void addKeyPress(String key) {
+		addIdentifier(key);
+		key = key.toUpperCase();
+		panel.getInputMap().put(KeyStroke.getKeyStroke(key), "pressed");
+	}
+
+	public void addKeyRelease(String key) {
+		addIdentifier(key);
+		key = "released " + key.toUpperCase();
+		panel.getInputMap().put(KeyStroke.getKeyStroke(key), "released");
+	}
+
+	public void addKey(String key) {
+		addKeyPress(key);
+		addKeyRelease(key);
+	}
+
+	private void addIdentifier(String key) {
+		for (String i : keysValues) {
+			if (i.equals(key)) {
+				return;
+			}
+		}
+
+		String[] keysNew = new String[keysValues.length + 1];
+		for (int i = 0; i < keysValues.length; i++) {
+			keysNew[i] = keysValues[i];
+		}
+		keysNew[keysValues.length] = key;
+		keysValues = keysNew;
+	}
+}
