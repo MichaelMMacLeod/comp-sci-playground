f2ce07a715dabd6eb8273cbf27d3c26beeefa99d
Michael MacLeod <michaelmmacleod@gmail.com>
2016-12-14 15:01:07 -0800

    Draw a triangle

--- Java/SampleGraphics/MyPanel.java
+++ Java/SampleGraphics/MyPanel.java
@@ -1,21 +1,23 @@
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Dimension;
+import java.awt.Polygon;
 
 public class MyPanel extends JPanel {
 
-	private int width, height;
+	private int width, height, iterations;
 	private InputManager input;
 
 	public MyPanel(double width, double height) {
 		this.width = (int) width;
 		this.height = (int) height;
 
+		iterations = 0;
+
 		this.setFocusable(true);
 		this.requestFocus();
 
 		input = new InputManager(this);
-		input.addKey("a");
 
 		restart();
 	}
@@ -24,8 +26,24 @@ public class MyPanel extends JPanel {
 	
 	public void updateLogic() {}
 	
+	public Polygon snowflake() {
+		Polygon poly = new Polygon();
+		int size = 400;
+
+		int cx = width / 2, cy = height / 2;
+		double a = size / Math.sqrt(3), b = size;
+
+		poly.addPoint((int) (cx - b), (int) (cy + a));
+		poly.addPoint((int) (cx + b), (int) (cy + a));
+		poly.addPoint((int) cx, (int) (cy - Math.sqrt(a * a + b * b)));
+
+		return poly;
+	}
+
 	@Override
-	protected void paintComponent(Graphics g) {}
+	protected void paintComponent(Graphics g) {
+		g.drawPolygon(snowflake());
+	}
 	
 	@Override
 	public Dimension getPreferredSize() {
