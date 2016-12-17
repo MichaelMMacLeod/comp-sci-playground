3abe289633f99e0009a108f9008cfaf543721996
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-12-17 12:37:59 -0800

    Add the fractal project

---
+++ Java/Fractals/Fractals.java
@@ -0,0 +1,68 @@
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Fractals {
+
+    private static MyPanel myPanel;
+    private static final int MS_PER_UPDATE = 10;
+    private static boolean on = true;
+
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+        System.out.println("Hardware acceleration enabled: " + System.getProperty("sun.java2d.opengl"));
+    	System.out.println("Control(s): hold 'z' to cycle through number of recursive iterations");
+        
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+
+        myPanel = new MyPanel(width, height);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+
+        loop();
+    }
+
+    private static void loop() {
+        double previous = System.currentTimeMillis();
+        double lag = 0;
+
+        while (on) {
+            double current = System.currentTimeMillis();
+            double elapsed = current - previous;
+            previous = current;
+            lag += elapsed;
+
+            while (lag >= MS_PER_UPDATE) {
+                myPanel.updateLogic();
+                lag -= MS_PER_UPDATE;
+            }
+
+            myPanel.repaint();
+        }
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Snowflake");
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setUndecorated(false);
+
+        myPanel.setBackground(Color.BLACK);
+
+        frame.add(myPanel);
+        frame.pack();
+
+        // Center the window
+        frame.setLocationRelativeTo(null);
+        
+        frame.setVisible(true);
+    }
+}
---
+++ Java/Fractals/Pen.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics;
+
+public class Pen {
+
+	private double x, y, rot;
+	private Graphics g;
+
+	public Pen(Graphics g, double x, double y, double rot) {
+		this.g = g;
+		this.x = x;
+		this.y = y;
+		this.rot = rot;
+	}
+
+	// draws a straight line in the direction 'rot'
+	public void draw(double distance) {
+		double xNew = x + distance * Math.cos(rot * Math.PI / 180);
+		double yNew = y + distance * Math.sin(rot * Math.PI / 180);
+
+		g.drawLine((int) x, 
+			(int) y, 
+			(int) xNew, 
+			(int) yNew);
+
+		x = xNew;
+		y = yNew;
+	}
+
+	// change the direction the pen is facing
+	public void rotate(double angle) {
+		rot += angle;
+	}
+}
