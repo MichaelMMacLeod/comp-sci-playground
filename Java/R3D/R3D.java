d70cb704d7c254fa3466a1a5a4a3123b62548002
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-12-20 19:26:17 -0800

    Write out the full formula

--- Java/R3D/R3D.java
+++ Java/R3D/R3D.java
@@ -1,10 +1,10 @@
 public class R3D {
 
 	public static void main(String[] args) {
-		double[] a = {0, 0, 0};
+		double[] a = {10, 10, 10};
 		double[] c = {0, 0, 0};
-		double[] t = {0, 0, 0};
-		double[] e = {0, 0, 0};
+		double[] t = {0, 2, 0};
+		double[] e = {1, 1, 1};
 
 		double[] ans = project(a, c, t, e);
 		for (int i = 0; i < ans.length; i++) {
@@ -13,51 +13,35 @@ public class R3D {
 	}
 
 	// https://en.wikipedia.org/wiki/3D_projection
-	public static double[] project(double[] a, double[] c, double[] theta, double[] e) {
-		double[][] m1 = 
-		{
-			{1,  0,                  0                 },
-			{0,  Math.cos(theta[0]), Math.sin(theta[0])},
-			{0, -Math.sin(theta[0]), Math.cos(theta[0])}
-		};
-		double[][] m2 =
-		{
-			{Math.cos(theta[1]), 0, -Math.sin(theta[1])},
-			{0,                  1,  0                 },
-			{Math.sin(theta[1]), 0,  Math.cos(theta[1])}
-		};
-		double[][] m3 =
-		{
-			{ Math.cos(theta[2]), Math.sin(theta[2]), 0},
-			{-Math.sin(theta[2]), Math.cos(theta[2]), 0},
-			{0,                   0,                  1}
-		};
-		double[][] m4 =
-		{{
-			a[0] - c[0],
-			a[1] - c[1],
-			a[2] - c[2]
-		}};
+	public static double[] project(double[] a, double[] c, double[] t, double[] e) {
+		double tx = t[0], ty = t[1], tz = t[2];
+		double ax = a[0], ay = a[1], az = a[2];
+		double cx = c[0], cy = c[1], cz = c[2];
+		double ex = e[0], ey = e[1], ez = e[2];
 
-		double[][] d = mult(mult(m1, m2), m3);
-		double bx = e[2] / d[0][2] * d[0][0] - e[0];
-		double by = e[2] / d[0][2] * d[0][1] - e[1];
+		double ad = ax - cx;
+		double bd = ay - cy;
+		double cd = az - cz;
+
+		double dx = ad * Math.cos(ty) * Math.cos(tz)
+			+ bd * Math.cos(ty) * Math.sin(tz)
+			- cd * Math.sin(ty);
+		double dy = -ad * Math.cos(tx) * Math.sin(tz)
+			+ bd * Math.cos(tx) * Math.cos(tz)
+			+ cd * Math.sin(tx) * Math.cos(ty)
+			+ Math.sin(tx) * Math.sin(ty) * Math.cos(tz)
+			+ Math.sin(tx) * Math.sin(ty) * Math.sin(tz);
+		double dz = ad * Math.sin(tx) * Math.sin(tz)
+			- bd * Math.sin(tx) * Math.cos(tz)
+			+ cd * Math.cos(tx) * Math.cos(ty)
+			+ Math.cos(tx) * Math.sin(ty) * Math.cos(tz)
+			+ Math.cos(tx) * Math.sin(ty) * Math.sin(tz);
+
+		double bx = ez / dz * dx - ex;
+		double by = ez / dz * dy - ey;
 
 		double[] ans = {bx, by};
-		return ans;
-	}
 
-	public static double[][] mult(double[][] a, double[][] b) {
-		int m1 = a.length;
-        int n1 = a[0].length;
-        int m2 = b.length;
-        int n2 = b[0].length;
-        if (n1 != m2) throw new RuntimeException("Illegal matrix dimensions.");
-        double[][] c = new double[m1][n2];
-        for (int i = 0; i < m1; i++)
-            for (int j = 0; j < n2; j++)
-                for (int k = 0; k < n1; k++)
-                    c[i][j] += a[i][k] * b[k][j];
-        return c;
+		return ans;
 	}
 }
