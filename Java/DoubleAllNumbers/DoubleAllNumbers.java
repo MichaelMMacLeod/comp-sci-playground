2c3ab1b19e7edea06faa7b78322f314789f318d4
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-01-12 17:32:54 -0800

    Ask for input until user enters "exit"

--- Java/DoubleAllNumbers/DoubleAllNumbers.java
+++ Java/DoubleAllNumbers/DoubleAllNumbers.java
@@ -3,28 +3,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 
+// only works for integers
+// ex: 2.9 -> 4.18 instead of 2.9 -> 5.8
+
 public class DoubleAllNumbers {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String s = scan.nextLine();
 
-		ArrayList<String> sa = new ArrayList<String>();
+		String s = "";
 
-		Matcher m = Pattern.compile("\\D+|\\d+").matcher(s);
+		while (!s.equalsIgnoreCase("exit")) {
+			System.out.print("Type in a sentence and press ENTER. ");
+			s = scan.nextLine();
 
-		while (m.find()) {
-			sa.add(m.group());
+			ArrayList<String> list = new ArrayList<String>();
+
+			Matcher m = Pattern.compile("\\D+|\\d+").matcher(s);
+
+			while (m.find()) {
+				list.add(m.group());
+			}
+
+			for (int i = 0; i < list.size(); i++) {
+				try {
+					list.set(i, Integer.toString(2 * Integer.parseInt(list.get(i))));
+				} catch (Exception e) {}
+			}
+
+			if (!s.equalsIgnoreCase("exit")) {
+				for (String i : list)
+					System.out.print(i);
+				System.out.println();
+			}
 		}
-
-		for (int i = 0; i < sa.size(); i++) {
-			try {
-				sa.set(i, Integer.toString(2 * Integer.parseInt(sa.get(i))));
-			} catch (Exception e) {}
-		}
-
-		for (String i : sa) 
-			System.out.print(i);
-		System.out.println();
 	}
 }
