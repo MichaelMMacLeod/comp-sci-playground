4b8d6b196951e400f1c8f8cb2ef18091b6b5f72b
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-01-17 16:56:05 -0800

    Improve camera zooming

--- Java/Sonder/Camera.java
+++ Java/Sonder/Camera.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 public class Camera {
 
     private int width, height;
-    private double x, y;
 
     private ArrayList<Drawn> focuses;
     private ArrayList<Drawn> objects;
@@ -24,6 +23,12 @@ public class Camera {
         Graphics2D g2d = (Graphics2D) g;
 
         for (Drawn d : objects) {
+            // Calculate radius of zoom circle
+            double radius = width < height ? width / 2.5 : height / 2.5;
+
+            // Calculate centroid of focuses
+            double x = 0;
+            double y = 0;
             for (Drawn f : focuses) {
                 x += f.getX();
                 y += f.getY();
@@ -31,27 +36,45 @@ public class Camera {
             x /= focuses.size();
             y /= focuses.size();
 
-            double zoom = 500 / Math.sqrt(x * x + y * y);
-            if (zoom > 1.5) zoom = 1.5;
+            // Calculate furthest focus from centroid
+            double furthest = 1;
+            for (Drawn f : focuses) {
+                double a = f.getX() - x;
+                double b = f.getY() - y;
+                double c = Math.sqrt(a * a + b * b);
+                if (c > furthest) {
+                    furthest = c;
+                }
+            }
 
+            // Calculate zoom factor
+            double zoom = radius / furthest;
+
+            // Get shape vertices
             double[] xv = d.getXVerts();
             double[] yv = d.getYVerts();
+
+            // Get shape coordinates
             double xp = d.getX();
             double yp = d.getY();
+
+            // Get shape rotation
             double r = d.getRotation();
 
-            double cx = 0, cy = 0;
+            // Calculate zoomed in and translated points
+            double newX = 0, newY = 0;
             for (int i = 0; i < xv.length; i++) {
                 xv[i] = (xv[i] + xp - x) * zoom + width / 2;
                 yv[i] = (yv[i] + yp - y) * zoom + height / 2;
 
-                cx += xv[i];
-                cy += yv[i];
+                newX += xv[i];
+                newY += yv[i];
             }
-            cx /= xv.length;
-            cy /= yv.length;
+            newX /= xv.length;
+            newY /= yv.length;
 
-            g2d.rotate(r, cx, cy);
+            // Rotate shape around its centroid
+            g2d.rotate(r, newX, newY);
 
             int[] xvInt = new int[xv.length];
             int[] yvInt = new int[yv.length];
@@ -60,10 +83,12 @@ public class Camera {
                 yvInt[i] = (int) yv[i];
             } 
 
+            // Drawn shape
             g.setColor(d.getColor());
             g.drawPolygon(xvInt, yvInt, xv.length);
 
-            g2d.rotate(-r, cx, cy);
+            // Reset rotation
+            g2d.rotate(-r, newX, newY);
         }
     }
 }
