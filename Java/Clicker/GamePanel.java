0821a5b6e9c86a3d5c39f944a2fd0bd78a7d3960
Michael MacLeod <michaelmmacleod@gmail.com>
2017-01-20 10:22:42 -0800

    Use opengl

--- Java/Clicker/GamePanel.java
+++ Java/Clicker/GamePanel.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 
 public class GamePanel extends JPanel {
+
 	private int width, height;
 
 	private InputManager input = new InputManager(this);
@@ -22,6 +23,8 @@ public class GamePanel extends JPanel {
 	private int carDoors;
 
 	public GamePanel(double width, double height) {
+		System.setProperty("sun.java2d.opengl", "true");
+		
 		this.width = (int) width;
 		this.height = (int) height;
 
