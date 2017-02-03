ddfd7df226fcbb7240f790635cd7e2ccb498237d
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-02-02 21:35:31 -0800

    Remove fluff; server just pings back the user's input from cmd line

---
+++ Java/ServerTest/GameClient.java
@@ -0,0 +1,46 @@
+// From http://cs.lmu.edu/~ray/notes/javanetexamples/
+
+import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import java.net.Socket;
+
+public class GameClient {
+
+    private BufferedReader in;
+    private PrintWriter out;
+    private Scanner scan;
+
+    public static void main(String[] args) throws Exception {
+        GameClient client = new GameClient();
+        client.connectToServer();
+    }
+
+    public void connectToServer() throws IOException {
+        scan = new Scanner(System.in);
+
+        System.out.print("Enter IP address of server: ");
+        String serverAddress = scan.nextLine();
+
+        Socket socket = new Socket(serverAddress, 9898);
+        in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Consume the initial welcoming messages from the server
+        for (int i = 0; i < 3; i++) {
+            System.out.println(in.readLine());
+        }
+
+        while (true) {
+            if (scan.hasNextLine()) {
+                out.println(scan.nextLine());
+            }
+            System.out.println(in.readLine());
+        }
+    }
+}
---
+++ Java/ServerTest/GameServer.java
@@ -0,0 +1,67 @@
+// From http://cs.lmu.edu/~ray/notes/javanetexamples/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GameServer {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Server started.");
+        int clientNumber = 0;
+        ServerSocket listener = new ServerSocket(9898);
+        try {
+            while (true) {
+                new Game(listener.accept(), clientNumber++).start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
+
+    private static class Game extends Thread {
+        private Socket socket;
+        private int clientNumber;
+
+        public Game(Socket socket, int clientNumber) {
+            this.socket = socket;
+            this.clientNumber = clientNumber;
+            log("New connection with client# " + clientNumber + " at " + socket);
+        }
+
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                out.println("Hello, you are client #" + clientNumber + ".");
+                out.println("Enter a line with only a period to quit\n");
+
+                while (true) {
+                    String text;
+                    if ((text = in.readLine()) != null) {
+                        out.println(
+                            "input received by server: " + text);
+                    }
+                }
+            } catch (IOException e) {
+                log("Error handling client# " + clientNumber + ": " + e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    log("Error closing socket: " + e);
+                }
+                log("Connection with client# " + clientNumber + " closed");
+            }
+        }
+
+        private void log(String message) {
+            System.out.println(message);
+        }
+    }
+}
