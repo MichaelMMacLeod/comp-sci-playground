7fad4ed5a6fe7881b6cd529e7213e4662dda4429
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-03-06 18:11:05 -0800

    Override makeMove instead of act

--- Java/GridWorldSceensaver/RockDropper.java
+++ Java/GridWorldSceensaver/RockDropper.java
@@ -4,18 +4,25 @@ import info.gridworld.world.*;
 import java.util.ArrayList;
 
 public class RockDropper extends Critter {
+    // @Override
+    // public void act() {
+    //     Location current = getLocation();
+
+    //     if (getGrid() == null)
+    //         return;
+    //     ArrayList<Actor> actors = getActors();
+    //     processActors(actors);
+    //     ArrayList<Location> moveLocs = getMoveLocations();
+    //     Location loc = selectMoveLocation(moveLocs);
+    //     makeMove(loc);
+
+    //     new Rock(getColor()).putSelfInGrid(getGrid(), current);
+    // }
+
     @Override
-    public void act() {
-        Location current = getLocation();
+    public void makeMove(Location l) {
+    	super.makeMove(l);
 
-        if (getGrid() == null)
-            return;
-        ArrayList<Actor> actors = getActors();
-        processActors(actors);
-        ArrayList<Location> moveLocs = getMoveLocations();
-        Location loc = selectMoveLocation(moveLocs);
-        makeMove(loc);
-
-        new Rock(getColor()).putSelfInGrid(getGrid(), current);
+    	new Rock(getColor()).putSelfInGrid(getGrid(), l);
     }
 }
