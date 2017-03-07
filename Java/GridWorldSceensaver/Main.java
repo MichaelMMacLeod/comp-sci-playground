df2ec5e38eedd61567b5b030b5c5935a20c9b553
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-03-06 18:24:53 -0800

    Make rock droppers not drop rocks when they can't move

--- Java/GridWorldSceensaver/Main.java
+++ Java/GridWorldSceensaver/Main.java
@@ -7,12 +7,14 @@ import java.awt.Color;
 public class Main {
 	public static void main(String... args) {
 		ActorWorld world = new ActorWorld();
-		RockDropper rd = new RockDropper();
 
 		RockEater re = new RockEater();
 		re.setColor(Color.GREEN);
 
-		world.add(new Location(5, 5), rd);
+		world.add(new Location(5, 6), new RockDropper(Color.RED));
+		world.add(new Location(5, 7), new RockDropper(Color.BLUE));
+		world.add(new Location(5, 4), new RockDropper(Color.ORANGE));
+
 		world.add(new Location(5, 8), re);
 
 		world.show();
--- Java/GridWorldSceensaver/RockDropper.java
+++ Java/GridWorldSceensaver/RockDropper.java
@@ -1,15 +1,23 @@
 import info.gridworld.actor.*;
 import info.gridworld.grid.*;
 import info.gridworld.world.*;
+
 import java.util.ArrayList;
 
+import java.awt.Color;
+
 public class RockDropper extends Critter {
+	public RockDropper(Color color) {
+		setColor(color);
+	}
+
     @Override
     public void makeMove(Location l) {
     	Location current = getLocation();
 
     	super.makeMove(l);
 
-    	new Rock(getColor()).putSelfInGrid(getGrid(), current);
+    	if (current != l) 
+    		new Rock(getColor()).putSelfInGrid(getGrid(), current);
     }
 }
