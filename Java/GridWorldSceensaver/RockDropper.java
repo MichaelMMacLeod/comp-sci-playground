c04cd062c795a29b324c3bc5222265ba35d8fe96
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-03-06 18:13:06 -0800

    Implement changes from last commit

--- Java/GridWorldSceensaver/RockDropper.java
+++ Java/GridWorldSceensaver/RockDropper.java
@@ -4,25 +4,12 @@ import info.gridworld.world.*;
 import java.util.ArrayList;
 
 public class RockDropper extends Critter {
-    // @Override
-    // public void act() {
-    //     Location current = getLocation();
-
-    //     if (getGrid() == null)
-    //         return;
-    //     ArrayList<Actor> actors = getActors();
-    //     processActors(actors);
-    //     ArrayList<Location> moveLocs = getMoveLocations();
-    //     Location loc = selectMoveLocation(moveLocs);
-    //     makeMove(loc);
-
-    //     new Rock(getColor()).putSelfInGrid(getGrid(), current);
-    // }
-
     @Override
     public void makeMove(Location l) {
+    	Location current = getLocation();
+
     	super.makeMove(l);
 
-    	new Rock(getColor()).putSelfInGrid(getGrid(), l);
+    	new Rock(getColor()).putSelfInGrid(getGrid(), current);
     }
 }
