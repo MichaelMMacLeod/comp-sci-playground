67d7fdd0055c5fa9ea73bb74fd5b916f9afcc821
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-05-01 20:14:20 -0700

    Read weights from file

--- finalProject/src/Main.hs
+++ finalProject/src/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
 import Codec.Picture
+import Control.Monad
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+    image <- readImage "whatAmI.gif"
+    case image of
+        Left image -> do
+            classification <- classify image
+            print classification
+        Right image -> print "Image not recognized"
+
+weightFile :: IO [Double]
+weightFile = do
+    weights <- readFile "src/weights.txt"
+    return $ toDouble (lines weights)
+
+toDouble :: [String] -> [Double]
+toDouble = map read
+
+classify i = weightFile
