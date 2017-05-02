77ca16df75ec9fd0263c4bd7b4aa353487fe953c
Michael MacLeod <MichaelMMacLeod@Gmail.com>
2017-05-02 14:56:07 -0700

    Put the unclassified image in its own high level definition

--- finalProject/src/Main.hs
+++ finalProject/src/Main.hs
@@ -4,19 +4,25 @@ import Codec.Picture
 
 main :: IO ()
 main = do
-    image <- readImage "whatAmI.gif"
+    image <- unclassified
     case image of
         Left image -> do
             classification <- classify image
             print classification
         Right image -> print "Image not recognized"
 
-weightFile :: IO [Double]
-weightFile = do
-    weights <- readFile "src/weights.txt"
-    return $ toDouble (lines weights)
+weights :: IO [Double]
+weights = do
+    file <- readFile "src/weights.txt"
+    return $ toDouble (lines file)
+
+unclassified :: IO (Either String DynamicImage)
+unclassified = do
+    image <- readImage "whatAmI.gif"
+    return image
+
 
 toDouble :: [String] -> [Double]
 toDouble = map read
 
-classify i = weightFile
+classify i = weights
