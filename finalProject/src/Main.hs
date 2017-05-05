120f66c70216b0813f6457e81b39e4bbc4b4ba2c
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-05-04 17:41:26 -0700

    implement makeLayer and makeNet functions

--- finalProject/src/Main.hs
+++ finalProject/src/Main.hs
@@ -1,7 +1,35 @@
 module Main where
 
 main :: IO ()
-main = print . show $ net [1,2,3] [[0.2,0.3,0.7],[0.5]] act
+main = print . show $ makeNet inputs weights act (length weights - 1)
+
+inputs = [1]
+
+weights =
+    [
+        [
+            [0.1],
+            [0.2]
+        ],
+        [
+            [0.3,0.4]
+        ]
+    ]
+
+makeLayer :: [Double] -> [[Double]] -> (Double -> Double) -> Int -> [Double]
+makeLayer inputs weights f n
+    | n == length weights = []
+    | otherwise           = neuron inputs (weights !! n) f : next
+    where
+        next = makeLayer inputs weights f (n+1)
+
+makeNet :: [Double] -> [[[Double]]] -> (Double -> Double) -> Int -> [Double]
+makeNet inputs weights f n
+    | n == 0    = makeLayer inputs (head weights) f 0
+    | otherwise = makeLayer previous (weights !! n) f 0
+    where
+        previous = makeNet inputs weights f (n-1)
+
 
 -- placeholder for a more advanced function
 -- only works with one hidden node and one output node
