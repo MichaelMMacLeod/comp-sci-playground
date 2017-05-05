a51fe3afbe9a99cfc56d0d182b8bb5972c04cca8
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-05-05 11:18:12 -0700

    Remove the getLastLayer function

--- finalProject/src/Main.hs
+++ finalProject/src/Main.hs
@@ -5,11 +5,15 @@ main = do
     putStr "Input: "
     input <- readLn
     weights <- readFile "weights.txt"
-    -- putStrLn $ "Result: " ++ show (getLastLayer input (read weights))
     putStrLn $ "Result: " ++ show (getAllLayers input (read weights) act)
 
-getLastLayer :: [Double] -> [[[Double]]] -> [Double]
-getLastLayer inputs weights = getLastLayer' inputs weights act (length weights - 1)
+getAllLayers :: [Double] -> [[[Double]]] -> (Double -> Double) -> [[Double]]
+getAllLayers inputs weights f
+    | length weights == 1 = [makeLayer inputs (head weights) f]
+    | otherwise           = currentLayer : previousLayers
+    where
+        currentLayer   = makeLayer (last previousLayers) (last weights) f
+        previousLayers = getAllLayers inputs (init weights) f
 
 makeLayer :: [Double] -> [[Double]] -> (Double -> Double) -> [Double]
 makeLayer inputs weights f = makeLayer' inputs weights f 0
@@ -25,21 +29,6 @@ act' n = act n * (1 - act n)
 
 cost :: Floating a => a -> a -> a
 cost target actual = (1/2) * (target - actual) ** 2
-
-getLastLayer' :: [Double] -> [[[Double]]] -> (Double -> Double) -> Int -> [Double]
-getLastLayer' inputs weights f n
-    | n == 0    = makeLayer inputs (head weights) f
-    | otherwise = makeLayer previous (weights !! n) f
-    where
-        previous = getLastLayer' inputs weights f (n-1)
-
-getAllLayers :: [Double] -> [[[Double]]] -> (Double -> Double) -> [[Double]]
-getAllLayers inputs weights f
-    | length weights == 1 = [makeLayer inputs (head weights) f]
-    | otherwise           = currentLayer : previousLayers
-    where
-        currentLayer   = makeLayer (last previousLayers) (last weights) f
-        previousLayers = getAllLayers inputs (init weights) f
 
 makeLayer' :: [Double] -> [[Double]] -> (Double -> Double) -> Int -> [Double]
 makeLayer' inputs weights f n
