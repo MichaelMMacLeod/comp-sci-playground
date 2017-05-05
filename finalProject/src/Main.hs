b6ca87e3fb2d829d3955cebf4dcda42dc386ebe2
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-05-04 18:12:23 -0700

    Use helper functions with makeNet and makeLayer

--- finalProject/src/Main.hs
+++ finalProject/src/Main.hs
@@ -1,49 +1,61 @@
 module Main where
 
-main :: IO ()
-main = print . show $ makeNet inputs weights act (length weights - 1)
-
 inputs = [1]
 
 weights =
     [
         [
-            [0.1],
-            [0.2]
+            [0.1],[0.2]
         ],
         [
             [0.3,0.4]
         ]
     ]
 
--- Creates a layer of neurons using specified inputs, weights, and the
--- activation function, f.
-makeLayer :: [Double] -> [[Double]] -> (Double -> Double) -> Int -> [Double]
-makeLayer inputs weights f n
-    | n == length weights = []
-    | otherwise           = neuron inputs (weights !! n) f : next
-    where
-        next = makeLayer inputs weights f (n+1)
+main :: IO ()
+main = print . show $ makeNet inputs weights
 
--- Creates a network of neurons using specified inputs, weights, and the
--- activation function, f.
-makeNet :: [Double] -> [[[Double]]] -> (Double -> Double) -> Int -> [Double]
-makeNet inputs weights f n
-    | n == 0    = makeLayer inputs (head weights) f 0
-    | otherwise = makeLayer previous (weights !! n) f 0
-    where
-        previous = makeNet inputs weights f (n-1)
+-- Returns the result of passing inputs through a neural network created with
+-- the specified weights.
+makeNet :: [Double] -> [[[Double]]] -> [Double]
+makeNet inputs weights = makeNet' inputs weights act (length weights - 1)
 
+-- Returns the result of passing inputs through a layer of neurons created with
+-- the specified weights and activation function, f.
+makeLayer :: [Double] -> [[Double]] -> (Double -> Double) -> [Double]
+makeLayer inputs weights f = makeLayer' inputs weights f 0
+
+-- Returns the result of passing inputs into a neuron with the specified weights
+-- and activation function, f.
 neuron :: [Double] -> [Double] -> (Double -> Double) -> Double
 neuron inputs weights f = f (sum (zipWith (*) inputs weights))
 
--- activation function
+-- The logistic-sigmoid activation function.
 act :: Floating a => a -> a
 act n = 1 / (1 + exp 1 ** (-n))
 
--- derivative of the activation function
+-- The derivative of act.
 act' :: Floating a => a -> a
 act' n = act n * (1 - act n)
 
+-- The cost function.
 cost :: Floating a => a -> a -> a
 cost target actual = (1/2) * (target - actual) ** 2
+
+-- Helper function for makeNet; not meant to be called by anthing other than
+-- makeNet.
+makeNet' :: [Double] -> [[[Double]]] -> (Double -> Double) -> Int -> [Double]
+makeNet' inputs weights f n
+    | n == 0    = makeLayer inputs (head weights) f
+    | otherwise = makeLayer previous (weights !! n) f
+    where
+        previous = makeNet' inputs weights f (n-1)
+
+-- Helper function for makeLayer; not meant to be called by anything other than
+-- makeLayer.
+makeLayer' :: [Double] -> [[Double]] -> (Double -> Double) -> Int -> [Double]
+makeLayer' inputs weights f n
+    | n == length weights = []
+    | otherwise           = neuron inputs (weights !! n) f : next
+    where
+        next = makeLayer' inputs weights f (n+1)
