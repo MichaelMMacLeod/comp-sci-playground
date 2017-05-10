96557721fdd9f48e155df99b9a8e49ce675154a9
Michael MacLeod <MichaelMMacLeod@Gmail.com>
2017-05-10 12:50:39 -0700

    Make net apply the activation function instead of neuron

--- finalProject/src/Main.hs
+++ finalProject/src/Main.hs
@@ -57,23 +57,22 @@ writeRandSynapseNet = do
 net :: [Double] -> [[[Double]]] -> (Double -> Double) -> [[Double]]
 net _ [] _ = []
 net inputs (w:weights) f =
-    let currentLayer = layer inputs w f
-        nextLayers   = net currentLayer weights f
-    in  currentLayer : nextLayers
+    let currentLayer = map f (layer inputs w)
+        otherLayers  = net currentLayer weights f
+    in  currentLayer : otherLayers
 
 -- Returns the value of each neuron in a neuron layer created with the specified
 -- inputs, weights, and activation function, f.
-layer :: [Double] -> [[Double]] -> (Double -> Double) -> [Double]
-layer _ [] _ = []
-layer inputs (w:weights) f =
-    let currentNeuron = neuron inputs w f
-        nextNeurons   = layer inputs weights f
+layer :: [Double] -> [[Double]] -> [Double]
+layer _ [] = []
+layer inputs (w:weights) =
+    let currentNeuron = neuron inputs w
+        nextNeurons   = layer inputs weights
     in  currentNeuron : nextNeurons
 
--- Returns the value of a neuron created with the specified inputs, weights, and
--- activaiton function, f.
-neuron :: [Double] -> [Double] -> (Double -> Double) -> Double
-neuron inputs weights f = f (sum (zipWith (*) inputs weights))
+-- Returns the value of a neuron created with the specified inputs and weights
+neuron :: [Double] -> [Double] -> Double
+neuron inputs weights = sum (zipWith (*) inputs weights)
 
 -- Generates a random network of synapses using the number of neurons in each
 -- layer and the range of an activation function, f.
