2c587c97bbedbc415872fdedf7f1279164f42662
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-05-28 18:56:11 -0700

    Add methods to serialize and deserialize objects

--- Java/Net/NetMatrix.java
+++ Java/Net/NetMatrix.java
@@ -1,4 +1,56 @@
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.Serializable;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.io.File;
+
 class NetMatrix {
+    static final String FILE_INPUT = "input.serialized";
+    static final String FILE_TARGET = "target.serialized";
+    static final String FILE_WEIGHTS = "weights.serialized";
+
+    static Object deSerialize(String filePath) {
+        try {
+            FileInputStream fin = new FileInputStream(filePath);
+            ObjectInputStream oin = new ObjectInputStream(fin);
+
+            Object s = oin.readObject();
+
+            fin.close();
+            oin.close();
+
+            return s;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    static void serialize(Serializable s, String filePath) {
+        try {
+            FileOutputStream fout = new FileOutputStream(filePath);
+            ObjectOutputStream oout = new ObjectOutputStream(fout);
+            
+            oout.writeObject(s);
+
+            fout.close();
+            oout.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         double[][] input = new double[][] {
             new double[] {1, 1, 0, 0},
@@ -8,7 +60,6 @@ class NetMatrix {
         double[][] target = new double[][] {
             new double[] {1, 0, 0, 0}
         };
-
         double[][][] weights = getRandomWeights(2, 4, 1);
 
         double learningRate = 0.25;
