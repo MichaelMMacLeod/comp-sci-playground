2235a486e17508ff963f1faa7de159445b67184b
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-05-28 19:07:25 -0700

    Migrate serialization methods to their own class

--- Java/Net/NetMatrix.java
+++ Java/Net/NetMatrix.java
@@ -1,55 +1,7 @@
-import java.io.ObjectInputStream;
-import java.io.FileInputStream;
-import java.io.Serializable;
-import java.io.ObjectOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-import java.io.File;
-
 class NetMatrix {
     static final String FILE_INPUT = "input.serialized";
     static final String FILE_TARGET = "target.serialized";
     static final String FILE_WEIGHTS = "weights.serialized";
-
-    static Object deSerialize(String filePath) {
-        try {
-            FileInputStream fin = new FileInputStream(filePath);
-            ObjectInputStream oin = new ObjectInputStream(fin);
-
-            Object s = oin.readObject();
-
-            fin.close();
-            oin.close();
-
-            return s;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    static void serialize(Serializable s, String filePath) {
-        try {
-            FileOutputStream fout = new FileOutputStream(filePath);
-            ObjectOutputStream oout = new ObjectOutputStream(fout);
-            
-            oout.writeObject(s);
-
-            fout.close();
-            oout.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         double[][] input = new double[][] {
---
+++ Java/Net/Serializer.java
@@ -0,0 +1,56 @@
+import java.io.Serializable;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+
+public final class Serializer {
+	/**
+	 * No need to instantiate a Serializer object.
+	 */
+	private Serializer() {}
+
+	/**
+	 * Reads a serialized object from a file.
+	 * 
+	 * @return the object located at filePath, or null, if filePath does not 
+	 *         exist.
+	 */
+	public static Object deSerialize(String filePath) {
+        try {
+            FileInputStream fin = new FileInputStream(filePath);
+            ObjectInputStream oin = new ObjectInputStream(fin);
+
+            Object s = oin.readObject();
+
+            fin.close();
+            oin.close();
+
+            return s;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Serializes and object and writes the serialization to filePath.
+     */
+    public static void serialize(Serializable s, String filePath) {
+        try {
+            FileOutputStream fout = new FileOutputStream(filePath);
+            ObjectOutputStream oout = new ObjectOutputStream(fout);
+            
+            oout.writeObject(s);
+
+            fout.close();
+            oout.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
